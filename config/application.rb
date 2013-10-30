8100f98adf5f5e4295c4c45765a136eaadebc132
Brian Robert Norberg <bnorberg02@gmail.com>
2013-10-30 09:58:42 -0400

    changed navbar names and added code to precompile

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module MyShawU
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # get precompilation working on heroku:
+    config.assets.initialize_on_precompile = true
   end
 end
