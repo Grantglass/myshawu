41341ba66745a2eaf0128c151c417a38f9180a85
Brian Norberg <bnorberg02@gmail.com>
2014-09-25 15:50:41 -0400

    changes to set environment variables for lentil to read in

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+APP_CONFIG = YAML.load(ERB.new(File.read("#{Rails.root}/config/lentil_config.yml")).result)[Rails.env]
+	
 # Initialize the rails application
 MyShawU::Application.initialize!
