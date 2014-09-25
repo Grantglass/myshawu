6f22bac9b39eaaa6210de5415052fad41c7dabc8
Brian Norberg <bnorberg02@gmail.com>
2014-09-25 11:10:36 -0400

    return to earlier git lentil_config file

--- config/initializers/instagram.rb
+++
@@ -1,8 +0,0 @@
-require "instagram"
-
-Instagram.configure do |config|
-  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
-  config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
-end
-
-CALLBACK_URL = "http://myshawu-staging.herokuapp.com/auth/google_oauth2/callback"
