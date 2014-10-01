3f236bc3819cf5ae96923911f9c9276b3facebf6
Brian Norberg <bnorberg02@gmail.com>
2014-10-01 12:03:09 -0400

    revert to original config settings

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
-APP_CONFIG = YAML.load(ERB.new(File.read("#{Rails.root}/config/lentil_config.yml")).result)[Rails.env]
 	
 # Initialize the rails application
 MyShawU::Application.initialize!
