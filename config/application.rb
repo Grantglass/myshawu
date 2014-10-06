342adc63ddd13c51286d71fb30d00772f002674f
Brian Norberg <bnorberg02@gmail.com>
2014-10-06 12:36:06 -0400

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/lentil_config.yml")[RAILS_ENV]
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
